67e791920c916df509c43f00ebbd5c79d2d2b5c1
Peter Szemann <szemannp@gmail.com>
2018-01-14 19:01:49 +0100

    basic routing, adding propTypes

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,33 @@
 import React from 'react'
 
+import { getFunName } from '../helpers'
+
+import PropTypes from 'prop-types'
+
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault()
+
+    let storeId = this.storeInput.value
+
+    console.log("KUKI>>>>>>", this.props)
+    this.props.history.push(`store/${storeId}`)
+  }
+
   render() {
     return (
-      <form action="" className="store-selector">
+      <form action="" className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please enter a Store</h2>
-        <input type="text" required placeholder="Store Name"/>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
         <button type="submit">Visit Store &rarr;</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: PropTypes.object
 }
 
 export default StorePicker
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import './css/style.css'
 
 import App from './components/App'
+import StorePicker from './components/StorePicker'
 
-ReactDOM.render(<App />, document.getElementById('main'))
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('main'))
